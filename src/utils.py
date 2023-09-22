b9fbb8502f9ed3dd93e48e09a3a86c6b3164e968
Andrey <yuryev@yandex.ru>
2023-09-23 00:46:39 +0300

    add get_operations function

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,12 @@
-def get_operations(filename='operations.json'):
+import json
+import os
+
+
+def get_operations(filename='operations.json',folder='./'):
+    ''' Прочитать файл с выпиской операций и вернуть коллекцию '''
     operation_collection = {}
-    return operation_collection
+    filepath = os.path.join(filename, folder)
+    if os.path.isfile(filepath):
+        with open(file=filepath, encoding='utf-8', mode='r') as file:
+            operation_collection = json.load(file)
+        return operation_collection
