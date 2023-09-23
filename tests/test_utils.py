a96bc1fe2f2982e7ef4d4f6ffe1087ae31bf0fee
Andrey <yuryev@yandex.ru>
2023-09-23 15:19:53 +0300

    add test case for sort list

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,16 @@
-from src.utils import get_operations
+from src.utils import get_operations, get_sorted_list
 
 
 def test_file_is_exist():
     assert get_operations('operations.json', '../data') != None
     assert get_operations('operation.json', 'data') == None
+
+
+def test_get_sorted_list():
+    unsorted_list = [{"id": 1, "date": "2018-06-30T02:08:58.425572"},
+                     {"id": 2, "date": "2019-07-03T18:35:29.512364"},
+                     {"id": 3, "date": "2019-08-26T10:50:58.294041"}]
+    sorted_list = [{"id": 3, "date": "2019-08-26T10:50:58.294041"},
+                   {"id": 2, "date": "2019-07-03T18:35:29.512364"},
+                   {"id": 1, "date": "2018-06-30T02:08:58.425572"}]
+    assert get_sorted_list(unsorted_list) == sorted_list
