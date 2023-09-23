81ca9083d1a63513d6722f090b3087d2bbd1307c
Andrey <yuryev@yandex.ru>
2023-09-23 22:33:37 +0300

    add main logic

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
+import utils
+
+
 def main():
     ''' Основная функция '''
     # считать данные из файла
+    operations = utils.get_operations()
     # отсортировать по дате
+    sorted_operations = utils.get_sorted_list(operations)
     # взять пять последних записей
+    executed_operations = utils.get_last_executed(sorted_operations)
     # вывести записи в нужном формате
+    for operation in executed_operations:
+        pass
 
 
 if __name__ == '__main__':
