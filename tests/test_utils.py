ddb5d9190762f02fd0963be7e7a37f64281d5514
Andrey <yuryev@yandex.ru>
2023-09-24 00:18:41 +0300

    add new case for formatted operation

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -41,28 +41,50 @@ def test_last_operations():
 
 
 def test_get_formatted_operation():
-    operation = {"id": 441945886,
-                 "state": "EXECUTED",
-                 "date": "2019-08-26T10:50:58.294041",
-                 "operationAmount": {
-                     "amount": "31957.58",
-                     "currency": {
-                         "name": "руб.",
-                         "code": "RUB"
-                     }
-                 },
-                 "description": "Перевод организации",
-                 "from": "Maestro 1596837868705199",
-                 "to": "Счет 64686473678894779589"}
-    formatted_operation = ['26.08.2019',
-                           'Перевод организации',
-                           'Maestro',
-                           '1596837868705199',
-                           'Счет',
-                           '64686473678894779589',
-                           '31957.58',
-                           'руб.']
-    assert get_formatted_operation(operation) == formatted_operation
+    operation1 = {"id": 441945886,
+                  "state": "EXECUTED",
+                  "date": "2019-08-26T10:50:58.294041",
+                  "operationAmount": {
+                      "amount": "31957.58",
+                      "currency": {
+                          "name": "руб.",
+                          "code": "RUB"
+                      }
+                  },
+                  "description": "Перевод организации",
+                  "from": "Maestro 1596837868705199",
+                  "to": "Счет 64686473678894779589"}
+    formatted_operation1 = ['26.08.2019',
+                            'Перевод организации',
+                            'Maestro',
+                            '1596837868705199',
+                            'Счет',
+                            '64686473678894779589',
+                            '31957.58',
+                            'руб.']
+    assert get_formatted_operation(operation1) == formatted_operation1
+    operation2 = {"id": 895315941,
+                  "state": "EXECUTED",
+                  "date": "2018-08-19T04:27:37.904916",
+                  "operationAmount": {
+                      "amount": "56883.54",
+                      "currency": {
+                          "name": "USD",
+                          "code": "USD"
+                      }
+                  },
+                  "description": "Перевод с карты на карту",
+                  "from": "Visa Classic 6831982476737658",
+                  "to": "Visa Platinum 8990922113665229"}
+    formatted_operation2 = ['19.08.2018',
+                            'Перевод с карты на карту',
+                            'Visa Classic',
+                            '6831982476737658',
+                            'Visa Platinum',
+                            '8990922113665229',
+                            '56883.54',
+                            'USD']
+    assert get_formatted_operation(operation2) == formatted_operation2
     assert get_formatted_operation({}) == []
 
 
