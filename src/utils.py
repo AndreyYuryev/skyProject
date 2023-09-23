a9a4e101db8300d05ea228783258d2e05aeae622
Andrey <yuryev@yandex.ru>
2023-09-23 15:03:58 +0300

    change get_operation function

--- src/utils.py
+++ src/utils.py
@@ -2,11 +2,16 @@ import json
 import os
 
 
-def get_operations(filename='operations.json',folder='./'):
-    ''' Прочитать файл с выпиской операций и вернуть коллекцию '''
-    operation_collection = {}
+def get_operations(filename='operations.json', folder='data'):
+    '''
+    Прочитать файл с выпиской операций и вернуть список с операциями
+    :param filename имя json-файла
+    :param folder имя папки в структуре проекта где лежит json
+    :return список содержащий операции
+    '''
+    operation_list = []
     filepath = os.path.join(filename, folder)
     if os.path.isfile(filepath):
         with open(file=filepath, encoding='utf-8', mode='r') as file:
-            operation_collection = json.load(file)
-        return operation_collection
+            operation_list = json.load(file)
+        return operation_list
